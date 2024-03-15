f1e064b6ce1df1f89c40602688ff27047c363c5c
Halicksse <alix.carlhant.pro@gmail.com>
2024-03-15 17:08:07 +0100

    update: replaced again NavBar and Footer cause we are learning

--- app/ui/Navigation/Navbar.tsx
+++ app/ui/Navigation/Navbar.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 export default function NavBar() {
     return (
         <div className="text-yellow-300">
+            <Link href="/">Maison de Piou</Link>
             <Link href="/boutique">Piou Boutique</Link>
             <Link href="/profil">Piou Profil</Link>
         </div>
