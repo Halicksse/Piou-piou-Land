3250d8aa8b603714b71c3b7a418b316e1fb254fd
Halicksse <alix.carlhant.pro@gmail.com>
2024-03-15 14:48:23 +0100

    commit before starting for real

---
+++ app/lib/actions.ts
@@ -0,0 +1,21 @@
+// 'use server';
+
+// import { z } from 'zod';
+// // import { revalidatePath } from 'next/cache';
+// import { redirect } from 'next/navigation';
+// import { signIn } from '@/auth';
+// import { AuthError } from 'next-auth';
+
+// const FormSchema = z.object({
+//     email: z.string().email(),
+//     password: z.string().min(6),
+// })
+
+// const UpdateUser = 
+
+// export async function updateUser(
+//     id: string,
+//     formData: FormData,
+//     ) {
+//     userId : 
+// )
